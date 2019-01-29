f19ff6273efe43d43a217000f03eb0cd58c3cd9a
RastaPopoulos <rastapopoulos@spip.org>
2019-01-29 13:56:28 +0100

    The build for the previous add to customize button behavior.

--- dist/priority-nav.js
+++ dist/priority-nav.js
@@ -1,4 +1,10 @@
 /*
+ * priority-nav - v1.0.13 | (c) 2019 @gijsroge | MIT license
+ * Repository: https://github.com/gijsroge/priority-navigation.git
+ * Description: Priority+ pattern navigation that hides menu items if they don't fit on screen.
+ * Demo: http://gijsroge.github.io/priority-nav.js/
+ */
+/*
  * priority-nav - v1.0.13 | (c) 2018 @gijsroge | MIT license
  * Repository: https://github.com/gijsroge/priority-navigation.git
  * Description: Priority+ pattern navigation that hides menu items if they don't fit on screen.
